408d06b64179358355aba4de1b1567c8b08674d9
CarlosDev <carlossilvasilvadiaz@gmail.com>
2023-08-17 22:44:05 -0400

    Se implementa un login básico

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  usuario: string = '';
+  contrasena: string = '';
 
-  ngOnInit() {
+  constructor(private router: Router, private toastController: ToastController) {}
+
+  async ingresar() {
+    if (this.usuario.length >= 3 && this.usuario.length <= 8 && /^\d{4}$/.test(this.contrasena)) {
+      
+      this.router.navigate(['/home'], {
+        queryParams: {
+          usuario: this.usuario,
+          contrasena: this.contrasena
+        }
+      });
+    } else {
+      
+      const toast = await this.toastController.create({
+        message: 'Usuario o contraseña inválidos',
+        duration: 1000, // Duración en milisegundos
+        color: 'danger' // Puedes ajustar el color según tus estilos
+      });
+
+      await toast.present();
+    }
   }
 
 }
